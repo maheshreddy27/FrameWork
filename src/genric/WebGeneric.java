9d32f37cf32a430f0a4ff494191c24e18b552f10
Mahesh <Mahesh@LAPTOP-1C7E9GHN>
2018-10-12 08:48:30 +0530

    Second Test

---
+++ src/genric/WebGeneric.java
@@ -0,0 +1,5 @@
+package genric;
+
+public class WebGeneric {
+
+}
--- src/testcases/TC_01.java
+++ src/testcases/TC_01.java
@@ -1,5 +1,11 @@
 package testcases;
 
-public class TC_01 {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
+public class TC_01 {
+@Test
+public void testa() {
+	Reporter.log("one",true);
+}
 }
